6fff7421ffdb64eedb1b1f5aa9734f01601fe0af
jjacinta <31937976+jjacinta@users.noreply.github.com>
2020-12-26 17:53:14 -0800

    Update background.js

--- stuff/background.js
+++ stuff/background.js
@@ -1,1 +1,16 @@
+/* Event: Runs when extension is installed */
+chrome.runtime.onInstalled.addListener(function() {
 
+  // Set which URL the extension can run on
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'bitcoinhero.me' }
+        }),
+      ],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+
+});
