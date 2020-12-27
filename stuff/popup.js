9b3291c47db6996e62f3df4ef76f3714b5cec8ab
jjacinta <31937976+jjacinta@users.noreply.github.com>
2020-12-26 18:19:19 -0800

    Update popup.js

--- stuff/popup.js
+++ stuff/popup.js
@@ -1,11 +1,14 @@
+
 let btnStart = document.getElementById('start-button');
 let btnStop = document.getElementById('stop-button');
 let lblStatus = document.getElementById('status-label');
 
 btnStart.onclick = function(element) {
   lblStatus.innerHTML = "Started";
+  chrome.runtime.sendMessage({action: "start"}, function(response) {});
 }
 
 btnStop.onclick = function(element) {
   lblStatus.innerHTML = "Stopped";
+  chrome.runtime.sendMessage({action: "stop"}, function(response) {});
 }
